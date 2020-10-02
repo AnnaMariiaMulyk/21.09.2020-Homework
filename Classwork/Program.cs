76a21c28c5590af39721956f68ee42da2314a3dc
AnnaMariiaMulyk <mulykannamariia@gmail.com>
2020-10-03 02:29:01 +0300

    second commit

--- Classwork/Program.cs
+++ Classwork/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,7 +20,7 @@ namespace Classwork
     }
     interface IFlyable
     {
-        float highOfFly { get; set; }
+        float HighOfFly { get; set; }
         void Fly();
     }
     interface ISwimmable
@@ -122,7 +123,15 @@ namespace Classwork
     }
     class Fish:Animal, ISwimmable
     {
-        protected string livingEnviroment { get; set; }
+        protected string LivingEnviroment { get; set; }
+        public Fish():base()
+        {
+            LivingEnviroment = "unknown";
+        }
+        public Fish(string name, string enviroment):base(name)
+        {
+            LivingEnviroment = enviroment;
+        }
         public float swimmingDepth
         {
             get
@@ -147,7 +156,238 @@ namespace Classwork
             Console.WriteLine("FISH");
             Console.WriteLine("Name: " + name);
             Console.WriteLine("Swimming depth: " + swimmingDepth);
-            Console.WriteLine("Living enviroment: " + livingEnviroment);
+            Console.WriteLine("Living enviroment: " + LivingEnviroment);
+        }
+    }
+    class Bird : Animal, IFlyable
+    {
+        public float HighOfFly
+        {
+            get
+            {
+                return HighOfFly;
+            }
+            set
+            {
+                if (value > 0)
+                    HighOfFly = value;
+                else
+                    HighOfFly = 0;
+            }
+        }
+        protected string ColorOfFeathers { get; set; }
+        public Bird():base()
+        {
+            HighOfFly = 0;
+            ColorOfFeathers = "unknown";
+        }
+        public Bird(string name, float heigh, string color):base(name)
+        {
+            HighOfFly = heigh;
+            ColorOfFeathers = color;
+        }
+        public void Fly()
+        {
+            Console.WriteLine("I`m flying with this height: " + HighOfFly);
+        }
+        public override void Print()
+        {
+            Console.WriteLine("BIRD");
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Height if fly: " + HighOfFly);
+            Console.WriteLine("Color of feathers: " + ColorOfFeathers);
+        }
+    }
+    class Bear:Mammal
+    {
+        private float Weight 
+        { 
+            get
+            {
+                return Weight;
+            }
+            set
+            {
+                if (value > 0)
+                    Weight = value;
+                else
+                    Weight = 0;
+            }
+        }
+        public Bear():base()
+        {
+            Weight = 0;
+        }
+        public Bear(string name, string color, float speed, float weight):base(name, color, speed)
+        {
+            this.Weight = weight;
+        }
+        public override void Print()
+        {
+            Console.WriteLine("BEAR");
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Speed of run: " + speedOfRun);
+            Console.WriteLine("Color of hair: " + colorOfHair);
+            Console.WriteLine("Weight: " + Weight);
+        }
+        public void Hunt()
+        {
+            Random rand = new Random();
+            int temp = rand.Next(1, 3);
+            switch (temp)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("Catching fish in river");
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("Looking for berries, mushrooms");
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine("Cathing small animal");
+                        break;
+                    }
+                default:
+                    break;
+            }
+        }
+    }
+    class Frog:Reptile
+    {
+        private string CountryOfSpread { get; set; }
+        public Frog():base()
+        {
+            CountryOfSpread = "unknown";
+        }
+        public Frog(string name, float speed, string country):base(name, speed)
+        {
+            CountryOfSpread = country;
+        }
+        public void JumpAndTalk()
+        {
+            Console.WriteLine("I`m jumping. KWA KWA KWA");
+        }
+        public override void Print()
+        {
+            Console.WriteLine("FROG");
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Speed of crow" + speedOfCrow);
+            Console.WriteLine("Cpuntry of spread: " + CountryOfSpread);
+        }
+
+    }
+    class Dolphin : Mammal, ISwimmable
+    {
+        private float Length
+        {
+            get
+            {
+                return Length;
+            }
+            set
+            {
+                if (value > 0)
+                    Length = value;
+                else
+                    Length = 0;
+            }
+        }
+        public Dolphin():base()
+        {
+            Length = 0;
+        }
+        public Dolphin(string name, string color, float speed, float length):base(name, color, speed)
+        {
+            Length = length;
+        }
+        public float swimmingDepth
+        {
+            get
+            {
+                return swimmingDepth;
+            }
+            set
+            {
+                if (value > 0)
+                    swimmingDepth = value;
+                else
+                    swimmingDepth = 0;
+            }
+        }
+        public void Swim()
+        {
+            Console.WriteLine("I am swimming. My swimming depth is: " + swimmingDepth);
+        }
+        public override void Print()
+        {
+            Console.WriteLine("DOLPHIN");
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Swimming depth: " + swimmingDepth);
+            Console.WriteLine("Length: " + Length);
+        }
+    }
+    class Carp:Fish
+    {
+        private float Weight
+        {
+            get
+            {
+                return Weight;
+            }
+            set
+            {
+                if (value > 0)
+                    Weight = value;
+                else
+                    Weight = 0;
+            }
+        }
+        public Carp():base()
+        {
+            Weight = 0;
+        }
+        public Carp(string name, string enviroment, float weight):base(name, enviroment)
+        {
+            Weight = weight;
+        }
+        public override void Print()
+        {
+            Console.WriteLine("CARP");
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Living enviroment: " + LivingEnviroment);
+            Console.WriteLine("Weight: " + Weight);
+        }
+        public void LayEggs()
+        {
+            Console.WriteLine("I`m laying eggs");
+        }
+    }
+    class Eagle:Bird
+    {
+        private int countOfKilledMouses;
+        public Eagle():base()
+        {
+            countOfKilledMouses = 0;
+        }
+        public Eagle(string name,float heigh, string color):base(name, heigh, color)
+        {
+            countOfKilledMouses = 0;
+        }
+        public override void Print()
+        {
+            Console.WriteLine("EAGLE");
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Height if fly: " + HighOfFly);
+            Console.WriteLine("Color of feathers: " + ColorOfFeathers);
+            Console.WriteLine("Count of killed mouses: " + countOfKilledMouses);
+        }
+        public void EatMouse()
+        {
+            countOfKilledMouses++;
         }
     }
     class Program
